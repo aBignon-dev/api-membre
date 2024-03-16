ad06e4a647cdf99494fe75b35f5c4a1955a9c161
Yan <yannmarcel43@gmail.com>
2024-03-16 22:29:57 +0100

    Update MembreDTO.java

--- src/main/java/membreapi/dto/MembreDTO.java
+++ src/main/java/membreapi/dto/MembreDTO.java
@@ -1,91 +1,40 @@
 package membreapi.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+/**
+ * DTO représentant les informations d'un membre.
+ */
+@Getter
+@Setter
 public class MembreDTO {
-	private String id;
 
-	@NotBlank(message = "Le prénom est requis")
-	private String prenom;
+    private String id; // Identifiant du membre
 
-	@NotBlank(message = "Le nom est requis")
-	private String nom;
+    @NotBlank(message = "Le prénom est requis")
+    private String prenom; // Prénom du membre
 
-	@NotNull(message = "La date de naissance est requise")
-	private LocalDate dateNaissance;
+    @NotBlank(message = "Le nom est requis")
+    private String nom; // Nom du membre
 
-	@NotBlank(message = "L'adresse est requise")
-	private String adresse;
+    @NotNull(message = "La date de naissance est requise")
+    private LocalDate dateNaissance; // Date de naissance du membre
 
-	@NotBlank(message = "L'adresse email est requise")
-	@Email(message = "L'adresse email doit être valide")
-	private String mail;
+    @NotBlank(message = "L'adresse est requise")
+    private String adresse; // Adresse du membre
 
-	@NotBlank(message = "Le mot de passe est requis")
-	@Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
-	private String motDePasse;
+    @NotBlank(message = "L'adresse email est requise")
+    @Email(message = "L'adresse email doit être valide")
+    private String mail; // Adresse email du membre
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public LocalDate getDateNaissance() {
-		return dateNaissance;
-	}
-
-	public void setDateNaissance(LocalDate dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-
-	public String getAdresse() {
-		return adresse;
-	}
-
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-
-	// Autres getters et setters si nécessaire
+    @NotBlank(message = "Le mot de passe est requis")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    private String motDePasse; // Mot de passe du membre
 }
