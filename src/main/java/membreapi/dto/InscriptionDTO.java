9cb2ab61527621b4c1579407eb01a79c755406da
Yan <yannmarcel43@gmail.com>
2024-03-16 22:32:15 +0100

    Update InscriptionDTO.java

--- src/main/java/membreapi/dto/InscriptionDTO.java
+++ src/main/java/membreapi/dto/InscriptionDTO.java
@@ -1,22 +1,11 @@
 package membreapi.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class InscriptionDTO {
     private Long membreId;
     private Long evenementId;
-
-    public Long getMembreId() {
-        return membreId;
-    }
-
-    public void setMembreId(Long membreId) {
-        this.membreId = membreId;
-    }
-
-    public Long getEvenementId() {
-        return evenementId;
-    }
-
-    public void setEvenementId(Long evenementId) {
-        this.evenementId = evenementId;
-    }
 }
